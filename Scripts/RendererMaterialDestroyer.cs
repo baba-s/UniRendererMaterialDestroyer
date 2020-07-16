3e309d5f7c72355c418f3458e5c38adcb8ba1186
baba_s <shotababa1206@gmail.com>
2020-07-16 22:32:16 +0900

    first commit

---
+++ Scripts/RendererMaterialDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal sealed class RendererMaterialDestroyer : MonoBehaviour
+	{
+		private void OnDestroy()
+		{
+			var renderers = GetComponentsInChildren<Renderer>( true );
+
+			for ( int i = 0; i < renderers.Length; i++ )
+			{
+				var materials = renderers[ i ].materials;
+
+				for ( int j = 0; j < materials.Length; j++ )
+				{
+					Destroy( materials[ j ] );
+				}
+			}
+		}
+	}
+}
